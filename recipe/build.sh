b90f8a7fe038e46fff4f46da950ce4fe9cfa36b2
XhstormR <10527522+XhstormR@users.noreply.github.com>
2026-07-06 20:54:25 +0800

    Update

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-set -ex
+set -exvo pipefail
+
+autoreconf -f -i
 
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* ./etc
-
-autoreconf -f -i
 
 if [[ ${target_platform} =~ linux.* ]]; then
   ./configure --prefix=$PREFIX --enable-sixel
